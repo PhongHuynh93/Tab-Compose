77a07868e4d17b3e3d587d7478eb82f938bdaf54
Do Nguyen <p7157568@gmail.com>
2021-06-26 21:07:45 +0700

    Create a pager with 4 tabs

---
+++ app/src/main/java/com/umbrella/tab/ui/tab/viewpager/TabWithPager.kt
@@ -0,0 +1,82 @@
+package com.umbrella.tab.ui.tab.viewpager
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabWithPager() {
+    val tabData = listOf(
+        "MUSIC" to Icons.Filled.Home,
+        "MARKET" to Icons.Filled.ShoppingCart,
+        "FILMS" to Icons.Filled.AccountBox,
+        "BOOKS" to Icons.Filled.Settings,
+    )
+    val pagerState = rememberPagerState(
+        pageCount = tabData.size,
+        initialOffscreenLimit = 2,
+        infiniteLoop = true,
+        initialPage = 1,
+    )
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            tabData.forEachIndexed { index, pair ->
+                Tab(selected = tabIndex == index, onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }, text = {
+                    Text(text = pair.first)
+                }, icon = {
+                    Icon(imageVector = pair.second, contentDescription = null)
+                })
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { index ->
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = tabData[index].first,
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TabWithPagerPreview() {
+    TabWithPager()
+}
