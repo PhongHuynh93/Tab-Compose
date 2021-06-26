cc0305d3403a765a24cda149e0c26dcb8dbcb5e4
Do Nguyen <p7157568@gmail.com>
2021-06-26 15:10:28 +0700

    Add pager library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-beta01")
+    implementation("com.google.accompanist:accompanist-pager:${rootProject.extra["pager_version"]}")
+    implementation("com.google.accompanist:accompanist-pager-indicators:${rootProject.extra["pager_version"]}")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
